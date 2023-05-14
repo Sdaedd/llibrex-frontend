34a09bed30fbcc85078af531c952ce3dd077f605
EddOll <74265420+sdaerdd@users.noreply.github.com>
2023-05-14 22:04:39 +0200

    update

--- src/components/CardLibro.vue
+++ src/components/CardLibro.vue
@@ -1,33 +1,23 @@
 <template>
-  <div class="carousel-container">
-    <div class="carousel">
-      <div class="carousel-item" v-for="libro in libros" :key="libro.id">
-        <div class="card has-background-black-ter has-text-white my-card">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img :src="libro.image" :alt="'Imagen de ' + libro.titulo" class="my-image">
-              <div class="image-overlay"></div>
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="content title is-6 has-text-light">
-              <p v-if="!showAll">{{ libro.title.substring(0, 19) }}
-                <a @click="showAll = true">...</a>
-              </p>
-              <p v-else>{{ libro.title }} <a @click="showAll = false">←</a></p>
-              <p class="subtitle is-7 has-text-grey">{{ libro.authors[0] }}<a v-if="libro.authors[1]" class="subtitle is-7 has-text-grey">{{ " & "+libro.authors[1].substring(0, 7)+"..."}}</a></p>
-            </div>
+  <div class="columns is-mobile is-multiline">
+    <div class="column is-one-fifth" v-for="libro in libros" :key="libro.id">
+      <div class="card has-background-black-ter has-text-white">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img :src="libro.image" alt="Imagen de {{libro.titulo}}" class="image">
+            <div class="image-overlay"></div>
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content title is-6 has-text-light">
+            <p v-if="!libro.showAll">{{ libro.title.substring(0, 22) }}
+              <a @click="libro.showAll = true">...</a>
+            </p>
+            <p v-else>{{ libro.title }} <a @click="libro.showAll = false">←</a></p>
+            <p class="subtitle is-7 has-text-grey">{{ libro.authors[0].replace(",", " & ") }}</p>
           </div>
         </div>
       </div>
-    </div>
-    <div class="carousel-navigation">
-      <button class="carousel-nav-prev" @click="prevSlide">
-        <font-awesome-icon :icon="['fas', 'user']" />
-      </button>
-      <button class="carousel-nav-next" @click="nextSlide">
-        <font-awesome-icon :icon="['fas', 'user']" />
-      </button>
     </div>
   </div>
 </template>
@@ -40,14 +30,13 @@ export default {
   data() {
     return {
       libros: [],
-      showAll: false,
       currentSlide: 0
     }
   },
   mounted() {
     axios.get('http://localhost:3000/libros')
       .then(response => {
-        this.libros = response.data;
+        this.libros = response.data.map(libro => ({ ...libro, showAll: false }));
       })
       .catch(error => {
         console.log(error);
@@ -69,11 +58,13 @@ export default {
 </script>
 
 <style>
-.my-card {
+.card {
   position: relative;
+  height: 290px;
+  width: 230px;
 }
 
-.my-image {
+.image {
   position: relative;
 }
 
@@ -83,53 +74,6 @@ export default {
   left: 0;
   right: 0;
   height: 100%;
-  background: linear-gradient(to top, hsl(0, 0%, 14%) 5%, rgba(0, 0, 0, 0));
-}
-
-.carousel-container {
-  position: relative;
-  overflow: hidden;
-}
-
-.carousel {
-  display: flex;
-  transition: transform 0.3s ease;
-}
-
-.carousel-item {
-  flex: 0 0 auto;
-}
-
-.carousel-navigation {
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  z-index: 1;
-}
-.carousel-nav-prev,
-.carousel-nav-next {
-background: transparent;
-border: none;
-color: white;
-font-size: 24px;
-padding: 0;
-cursor: pointer;
-transition: opacity 0.3s ease;
-}
-
-.carousel-nav-prev:hover,
-.carousel-nav-next:hover {
-opacity: 0.8;
-}
-
-.carousel-nav-prev {
-margin-left: 10px;
-}
-
-.carousel-nav-next {
-margin-right: 10px;
+  background: linear-gradient(to top, hsl(0, 0%, 14%, 100) 1%, rgba(0, 0, 0, 0));
 }
 </style>
